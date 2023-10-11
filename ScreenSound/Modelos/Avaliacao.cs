783bfe5e513ed102125a2bcb12d4c4d713be18dc
DieleCorrea <dybs1520@gmail.com>
2023-10-11 11:50:36 -0300

    Criei a classe Avaliação pois desejo encapsular as notas da lista notas da classe banda,, representando o conceito nota dentro dessa nova classe

---
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,15 @@
+﻿namespace ScreenSound.Modelos;
+
+//internal: visibilidade de classes. Apenas o PROJETO vai conseguir ver ela. Só passo para 
+//public se eu quiser que outros projetos vejam ela. O que não é o caso. 
+
+internal class Avaliacao
+{
+    //criar construtor
+    public Avaliacao(int nota) 
+    { 
+        Nota = nota;    
+    }
+    //propriedade acesso de leitura apenas
+    public int Nota { get; }
+}
