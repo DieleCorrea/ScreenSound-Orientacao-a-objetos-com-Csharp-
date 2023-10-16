38c6abb64cf71e090daad1f5822b8b2d0a71da3a
DieleCorrea <dybs1520@gmail.com>
2023-10-16 14:04:23 -0300

    Criei essa nova classe e fiz dela uma classe ancestral

---
+++ ScreenSound/Menus/Menu.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace ScreenSound.Menus
+{//CLASSE ANCESTRAL 
+    internal class Menu
+    {
+        public void ExibirTituloDaOpcao(string titulo)
+        {
+            int quantidadeDeLetras = titulo.Length;
+            string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+            Console.WriteLine(asteriscos);
+            Console.WriteLine(titulo);
+            Console.WriteLine(asteriscos + "\n");
+        }
+    }
+}
