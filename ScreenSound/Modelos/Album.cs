626c85c164e4f584e43a7e8d35c681cadfcc65e2
DieleCorrea <dybs1520@gmail.com>
2023-10-19 14:24:23 -0300

    implementei em album minha interface IAvaliaçaõ

--- ScreenSound/Modelos/Album.cs
+++ ScreenSound/Modelos/Album.cs
@@ -1,22 +1,34 @@
 ﻿namespace ScreenSound.Modelos;
 
-internal class Album
+internal class Album : IAvaliavel
 {
     private List<Musica> musicas = new List<Musica>();
+    private List<Avaliacao> notas = new List<Avaliacao>();
+
     //coonstrutor 
     public Album(string nome)
     {
         Nome = nome;
         ContadorDeObjetos++;
     }
-   
+
     public string Nome { get; }
     public int DuracaoTotal => musicas.Sum(m => m.Duracao);
     public List<Musica> Musicas => musicas;
+
+    public double Media
+    {
+        get
+        {
+            if (notas.Count == 0) return 0;
+            else return notas.Average(a => a.Nota);//verifico se a contagem não esta zerada, caso não esteja eu faço a média 
+        }
+    }
+
     public static int ContadorDeObjetos = 0;//campo estatico
 
-    
-   
+
+
     //metodos
     public void AdicionarMusica(Musica musica)
     {
@@ -31,5 +43,10 @@ internal class Album
             Console.WriteLine($"Música: {musica.Nome}");
         }
         Console.WriteLine($"\nPara ouvir este álbum inteiro você precisa de {DuracaoTotal}");
+    }
+
+    public void AdicionarNota(Avaliacao nota)
+    {
+        notas.Add(nota);
     }
 }
